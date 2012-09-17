f1ab7899527e51a74659e376d13a825bb14c0a0c
Joseph Kelly <jyklly@gmail.com>
2012-09-17 12:39:12 -0400

    ruby code now loops

--- municipalities.rb
+++ municipalities.rb
@@ -1,26 +1,35 @@
-# courtesy of https://github.com/cesarsalazar
+# some code contained courtesy of https://github.com/cesarsalazar
 
+require 'csv'
 require 'json'
 require 'net/http'
 require 'uri'
 
-#Example Data
-municipality = "Concepcion Papalo"
-state = "Oaxaca"
+#global
 country = "Mexico"
 
-#Perform a GET request to the Google Maps API for each municipality specifying also the state
-url = URI.parse('http://maps.google.com/maps/api/geocode/json?sensor=false&address='+URI.escape(municipality+","+state+","+country))
 
-#Results come in JSON format, so we convert them in a hash in order to retrieve the data
-response = JSON.parse(Net::HTTP.get(url).force_encoding('UTF-8'))
-results = response['results']  
-foo = results[0]
-geometry = foo['geometry']
-location = geometry['location']  
+CSV.open("data/latlon.csv", "wb") do |csv|
+  CSV.foreach("data/stateandmuni.csv") do |row|
+    parsed = row[0].split(/\t/)
+    state = parsed[0]
+    municipality = parsed[1]
+  
+    #Perform a GET request to the Google Maps API for each municipality specifying also the state
+    url = URI.parse('http://maps.google.com/maps/api/geocode/json?sensor=false&address='+URI.escape(municipality+","+state+","+country))
 
-#Finally LAT and LONG as strings
-lat = location['lat'].to_s
-long = location['lng'].to_s
+    #Results come in JSON format, so we convert them in a hash in order to retrieve the data
+    response = JSON.parse(Net::HTTP.get(url).force_encoding('UTF-8'))
+    results = response['results']  
+    foo = results[0]
+    geometry = foo['geometry']
+    location = geometry['location']  
+  
+    #Finally LAT and LONG as strings
+    lat = location['lat'].to_s
+    long = location['lng'].to_s
 
-puts lat, long
+    csv << [state,municipality,lat,long]
+  end
+end
+
